5098cceb11592f5ba2dc1bec67973768b93f0a2e
benlorenzetti <ben.lorenzetti@gmail.com>
2016-06-14 01:59:43 -0400

    auto_reserve, push_back, and at working with constructor demo

--- demo_vectlor.c
+++ demo_vectlor.c
@@ -6,14 +6,15 @@
 typedef struct city_s {
   char* name;
   int population;
-  float per_capita_income;
 } city;
 
-void city_copy_constructor(city* dest, const city* src) {
+int city_copy_constructor(city* dest, const city* src) {
   if(dest->name = malloc(1 + strlen(src->name)))
     strcpy(dest->name, src->name);
+  else
+    return -1; // define your own error codes less than zero.
   dest->population = src->population;
-  dest->per_capita_income = src->per_capita_income;
+  return 0; // success code must be zero
 }
 
 void godzilla(city* dest) { // godzilla, aka city destructor
@@ -22,4 +23,19 @@ void godzilla(city* dest) { // godzilla, aka city destructor
 
 int main() {
   veclor japanese_cities = VECLOR(city, city_copy_constructor, godzilla);
+//veclor japanese_cities = VECLOR(city, NULL, NULL);
+
+  char name[1000];
+  city c;
+  strcpy (c.name, "Tokyo");
+  c.population = 13510000;
+  veclor_push_back (&japanese_cities, &c);
+  strcpy (c.name, "Kyoto");
+  c.population = 1474000;
+  veclor_push_back (&japanese_cities, &c);
+
+  int i = 0;
+  city* city_ptr;
+  while (city_ptr = (city*) veclor_at(&japanese_cities, i++))
+    printf("%s has population %d\n", city_ptr->name, city_ptr->population);
 }
--- veclor.h
+++ veclor.h
@@ -1,13 +1,31 @@
 #ifndef VECLOR_H
 #define VECLOR_H
 
-#define VECLOR_START_SIZE 1
-#define VECLOR_ALLOC_BASE 1.5 
+#include <stdlib.h>
+#include <string.h>
+
+#define VECLOR_SUCCESS 0
+#define VECLOR_ALLOC_FAILURE 1
+
+/*   Constants A and B define the reservation policy for the automatic memory
+ * managenment. When the current reserved space x is insufficient, the array
+ * is reallocated to a new memory block of size
+ *
+ *                           x' = A*x + B
+ *
+ * Reserved space will also shrink according to: x' = (x - B) / A.
+ * 
+ *   Valid ranges for A and B are:
+ *     {A e Real: A >= 1}  (to ensure growth)
+ *     {B e Natural: B >= 1}  (to ensure growth for small x)
+*/
+#define VECLOR_A 1.3   // at large n, allocation size x ~ VECLOR_A^(n)
+#define VECLOR_B 1     // minimum allocation size, must be >= 1.
 
 #define VECLOR(template_type, constructor, destructor) \
 {                                                      \
   (sizeof(template_type)),                             \
-  ((void (*)(void*, const void*))(constructor)),       \
+  ((int (*)(void*, const void*))(constructor)),        \
   ((void (*)(void*))(destructor)),                     \
   0,                                                   \
   0,                                                   \
@@ -15,45 +33,81 @@
 }
 
 /* Create the type "veclor_copy_f" for a "pointer to a function with generic
- * dest and src pointer parameters and no return value".
+ * dest and src pointer parameters and integer return value".
  * Functions cast to this type can be registered as the copy constructor to
  * be used by any "STL" container. And a similar type for destructors.
 */
-typedef void (*veclor_copy_f) (void*, const void*); 
+typedef int (*veclor_copy_f) (void*, const void*); 
 typedef void (*veclor_dest_f) (void*);
 
 typedef struct veclor_s {
   int type_size;
   veclor_copy_f copy_constructor;
   veclor_dest_f destructor;
-  int alloc_size;
-  int array_size;
+  int alloc_size; // negative alloc_size indicates an explicit reservation that
+  int array_size;        // should not be shrunk by the auto_reserve() function
   void* begin;
 } veclor;
 
-void* reallocation_policy (veclor* dest) {
-  if (dest->begin == NULL)
+void* veclor_auto_reserve (veclor *);
+void* veclor_at (const veclor*, int);
+int veclor_push_back (veclor*, const void*);
+
+int veclor_push_back (veclor* vec, const void* val) {
+  // Ensure there is allocated space available for the new value
+  if (!veclor_auto_reserve(vec))
+    return VECLOR_ALLOC_FAILURE;
+  // Generate pointer to the new location and call the copy constructor
+  void* new_elem = vec->begin + vec->array_size * vec->type_size;
+  if (vec->copy_constructor)
   {
-    dest->begin = malloc(VECLOR_START_SIZE * dest->type_size);
-    dest->alloc_size = dest->begin ? VECLOR_START_SIZE : 0;
-    return dest->begin;
+    int cc_return = (*vec->copy_constructor) (new_elem, val);
+    if (cc_return)
+      return cc_return; // copy constructor failure code
   }
-  else if (dest->alloc_size == dest->array_size)
+  else // if no user specified constructor, default is a memory copy
   {
-    dest->begin = realloc(VECLOR_ALLOC_BASE * dest->alloc_size);
-    dest->alloc_size *= dest->begin ? VECLOR_ALLOC_BASE : 0;
-    return dest->begin;
+    memmove (new_elem, val, vec->type_size);
   }
-  else if (((dest->alloc_size - VECLOR_START_SIZE)/VECLOR_ALLOC_BASE) > dest->array_size)
+  // Increment the array size and return
+  vec->array_size++;
+  return VECLOR_SUCCESS;
+}
+
+
+void* veclor_at (const veclor* vec, int n) {
+  if (n >= vec->array_size)
+    return NULL; // out of bounds error
+  return vec->begin + n * vec->type_size;
+}
+
+void* veclor_auto_reserve (veclor* dest) {
+  // By default, assume that no resizing or relocation will be done-----------|
+  int new_alloc_size = dest->alloc_size;
+  void* new_alloc_ptr = dest->begin;
+  // Test if more memory needs to be reserved---------------------------------|
+  if (dest->array_size == abs(dest->alloc_size))   // Note: negative alloc_size
+  {                                     // indicates it was explicitly reserved
+    new_alloc_size = VECLOR_A * abs(dest->alloc_size) + VECLOR_B;
+    new_alloc_ptr = realloc(dest->begin, new_alloc_size * dest->type_size);
+    if (!new_alloc_ptr) // realloc() failure does not disturb original memory
+      return NULL;// so return NULL but dont overwrite dest->begin/alloc_size 
+  }
+  // Test if memory should be released----------------------------------------|
+  // It will never be if alloc_size is negative due to an explicit reservation
+  else if (dest->array_size < (dest->alloc_size - VECLOR_B) / VECLOR_A)
   {
-    void* new_begin = realloc(dest->alloc_size / VECLOR_ALLOC_BASE);
-    if (!new_begin)
-      return dest->begin; // realloc() leaves original block untouched on failure
-    dest->alloc_size = dest->alloc_size / VECLOR_ALLOC_BASE;
-    return (dest->begin = new_begin);
+    int new_size = (dest->alloc_size - VECLOR_B) / VECLOR_A;
+    new_alloc_ptr = realloc(dest->begin, new_alloc_size * dest->type_size);
+    if (!new_alloc_ptr)
+      return dest->begin; // failing to shrink the array isnt really a failure
   }
+  // Otherwise the current reservation size is ok so no changes---------------|
   else
-    return dest->begin;
+  {}
+  // Save changes and return pointer to the current location of the data------|
+  dest->alloc_size = new_alloc_size;
+  return (dest->begin = new_alloc_ptr);
 }
 
 #endif
